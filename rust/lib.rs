bf9f796de32dd4bbd87d71921592f272440400d0
Insu Jang <insujang@umich.edu>
2024-03-28 14:36:29 +0000

    Move planning components into planning directory Rust version bump up

--- rust/lib.rs
+++ rust/lib.rs
@@ -3,7 +3,7 @@ mod execution_result;
 mod pipeline_template_generator;
 use env_logger;
 use pyo3::prelude::*;
-use pyo3::types::PyDict;
+use pyo3::types::PyList;
 use std::fmt;
 use std::path::PathBuf;
 
@@ -47,28 +47,31 @@ fn create_pipeline_templates(
     generator.divide_and_conquer(num_nodes[num_nodes.len() - 1])?;
 
     Python::with_gil(|py| {
-        let results = PyDict::new(py);
+        let mut results: Vec<PyObject> = Vec::new();
 
-        let module = PyModule::import(py, "oobleck_colossalai.pipeline_template")?;
-        let class = module.getattr("PipelineTemplate")?;
+        let module = PyModule::import_bound(py, "oobleck_colossalai.pipeline_template")?;
+        let class = module.getattr("PipelineTemplate")?.into_py(py);
 
         for num_node in num_nodes {
             let template = generator.get_pipeline_template(num_node).unwrap();
-            let py_template = class.call1((
-                model_name.as_str(),
-                template.get_modules_per_stage(&generator.layer_execution_results),
-                template.latency(),
-                template.mem_required(),
-            ))?;
-            results.set_item(num_node, py_template.to_object(py))?;
+            let py_template = class.call1(
+                py,
+                (
+                    model_name.as_str(),
+                    template.get_modules_per_stage(&generator.layer_execution_results),
+                    template.latency(),
+                    template.mem_required(),
+                ),
+            )?;
+            results.push(py_template.to_object(py));
         }
 
-        Ok(results.to_object(py))
+        Ok(PyList::new_bound(py, results).to_object(py))
     })
 }
 
 #[pymodule]
-fn planner(_py: Python, m: &PyModule) -> PyResult<()> {
+fn planner(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     let _ = env_logger::try_init();
     m.add_function(wrap_pyfunction!(create_pipeline_templates, m)?)?;
     Ok(())
